e3967865e82364305e41a8dbb4b57efc720c33c7
FoxyClan <foxyclan.contract@gmail.com>
2024-10-26 14:31:08 +0200

    error component

--- src/app/app-component/app.routes.ts
+++ src/app/app-component/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/page-home/home.component';
 import { CollectionComponent } from '../components/page-collection/collection.component';
+import { ErrorComponent } from '../components/page-error/error.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'ErrorPage', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'collection', component: CollectionComponent },
+  { path: 'ErrorPage', component: ErrorComponent },
   { path: '**', redirectTo: 'ErrorPage', pathMatch: 'full'}
 ];
