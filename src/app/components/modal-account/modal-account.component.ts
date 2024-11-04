1404691a8a84dc0fe03b8d85ce96cf23826acc9e
FoxyClan <foxyclan.contract@gmail.com>
2024-11-04 17:11:06 +0100

    sort balance eth weth ok

--- src/app/components/modal-account/modal-account.component.ts
+++ src/app/components/modal-account/modal-account.component.ts
@@ -25,8 +25,8 @@ export class ModalAccount implements OnInit, OnDestroy {
   public selectedWallet: string = '';
   private subscription: Subscription;
   selectedOption: string = 'Token';
-  balance: string | null = null;
-  wethBalance: string | null = null;
+
+  balances: { symbol: string, balance: string }[] = [];
 
   constructor(private web3Service: Web3Service) {
     this.subscription = new Subscription();
@@ -39,7 +39,12 @@ export class ModalAccount implements OnInit, OnDestroy {
     ]).subscribe(([networkId, selectedWallet]) => {
       this.networkId = networkId;
       this.selectedWallet = selectedWallet;
-      this.loadBalance();
+      
+      this.loadBalance().then(balances => {
+        this.balances = balances;
+      }).catch((error) => {
+        console.error("Error loading balances:", error);
+      });
     });
   }
 
@@ -83,23 +88,49 @@ export class ModalAccount implements OnInit, OnDestroy {
   }
 
   
-  loadBalance() {
-    this.web3Service.getBalance().then((balance) => {
-      this.balance = balance;
-    }).catch((error) => {
-      console.error("Error fetching ETH balance:", error);
-    });
-    this.web3Service.getWethBalance().then((balance) => {
-      this.wethBalance = balance;
-    }).catch((error) => {
-      console.error("Error fetching WETH balance:", error);
+  async loadBalance(): Promise<{ symbol: string, balance: string }[]> {
+    let balances: { symbol: string, balance: string }[] = [];
+    return Promise.all([
+      this.web3Service.getBalance().then((balance) => {
+          balances.push({ symbol: "ETH", balance: balance });
+      }).catch((error) => {
+          console.error("Error fetching ETH balance:", error);
+      }),
+      
+      this.web3Service.getWethBalance().then((balance) => {
+          balances.push({ symbol: "WETH", balance: balance });
+      }).catch((error) => {
+          console.error("Error fetching WETH balance:", error);
+      })
+    ]).then(() => {
+        return this.sortBalances(balances);
     });
   }
 
-  convertBalance(numberStr: string | null, symbole: string) {
-    if(!numberStr) return "Loading ...";
-    console.log(this.wethBalance)
-    const formattedNumber = parseFloat(numberStr).toFixed(3);
+
+  convertBalance(balances: { symbol: string, balance: string }) {
+    if(!balances) return "Loading ...";
+    const numberStr = balances.balance;
+    const symbole = balances.symbol;
+    if(parseFloat(numberStr) > 0 && parseFloat(numberStr) < 0.001) return "< 0.001 " + symbole;
+    let formattedNumber = (Math.floor(parseFloat(numberStr) * 1000) / 1000).toString();
+    if(formattedNumber === "0.000") formattedNumber = "0";
     return formattedNumber + " " + symbole;
   }
+
+
+  sortBalances(balances: { symbol: string, balance: string }[]) {
+    const sortedBalances = balances
+      .map(item => ({
+        ...item,
+        balance: parseFloat(item.balance)
+      }))
+      .sort((a, b) => b.balance - a.balance)
+      .map(item => ({
+        ...item,
+        balance: item.balance.toString()
+      }));
+      return sortedBalances;
+  }
+
 }
