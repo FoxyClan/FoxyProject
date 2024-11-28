82aa0f9c724c74b2cbd1c5f1a8166e1814dd2c27
Mathieu Cazaux <matcaz33.mobile@gmail.com>
2024-11-28 19:10:52 +0100

    transfer

--- jar/src/main/java/com/foxyclan/jar/JarApplication.java
+++ jar/src/main/java/com/foxyclan/jar/JarApplication.java
@@ -2,6 +2,9 @@ package com.foxyclan.jar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Map;
+import java.util.HashMap;
+
 
 @SpringBootApplication
 public class JarApplication {
@@ -9,7 +12,14 @@ public class JarApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(JarApplication.class, args);
 		Mint mint = new Mint();
-		mint.NFT();
+		Map<String, String> adn = new HashMap<>();
+		adn.put("head covering", "06");
+		adn.put("mouth", "05");
+		adn.put("eyes", "04");
+		adn.put("clothes", "03");
+		adn.put("fur", "02");
+		adn.put("background", "01");
+		mint.NFT(adn);
 	}
 
 }
