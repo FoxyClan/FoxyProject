8d9052df13f200c3abe285acc6a102cb57430875
Mathieu Cazaux <matcaz33.mobile@gmail.com>
2025-02-23 11:28:53 +0100

    transfert

---
+++ FoxyAward.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.28;
+
+
+/**
+ * @title FoxyClan contract
+ * @dev Extends ERC721 Non-Fungible Token Standard basic implementation
+ */
+contract FoxyAward  {
+
+    function toto() public pure returns(string memory){
+        string memory tutu = "salut";
+        return tutu;
+    }
+}
